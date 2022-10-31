ce59d9b1e138ded5c9c72b78ff01e6161119684d
ainulfurkan <m.ainulfurkan@gmail.com>
2022-10-31 04:18:21 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>replit</title>
+  <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+  <?php echo "<h1>Sekolah Koding</h1>";
+          echo "Hallo<br>";
+          echo "Member"."<br>";
+
+    ?> 
+        Hallo Semua!!
+    <?php
+          // pertemuan ke 3
+        // variabel pada PHP
+        $nama = "Sekolah Koding 123";
+        $al = "Tex baru";
+
+       
+        echo  "halo" . $nama . "<br>";
+        echo  "Member";
+    ?>
+
+    <?php
+      // variabel pada PHP
+      // tipe data -string-
+      $nama = "Sekolah koding";
+      $nama2 = 'Bermain koding';
+      echo  "Selamat datang di $nama"."<br>";
+
+      //tipe data angka/number
+      // 1. integer 2. float
+      $angka = 1000;
+      $angka2 = 100.123;
+
+      echo $angka + $angka2;
+    ?>
+
+    <?php 
+</body>
+
+</html>
